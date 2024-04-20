47951367788c14875388ba1052c36acf8b2315f8
jaswanth chodavarapu <jaswanth.chodavarapu@buildbot.tech>
2024-04-20 22:47:03 +0530

    Added prettier and updated eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,12 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "routeTree.gen.ts"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "unused-imports"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "no-unused-vars": "warn",
+    "unused-imports/no-unused-imports": "warn",
   },
-}
+};
