803ec5ecb8d5741d806fbd51438c8a3c65236c1d
jaswanth chodavarapu <jaswanth.chodavarapu@buildbot.tech>
2024-04-20 20:06:49 +0530

    Added tanstack router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,21 @@
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import React from "react";
+const TanStackRouterDevtools =
+  import.meta.env.MODE === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
